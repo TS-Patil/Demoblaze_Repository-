17f5a1a85310b82b0ba08c87c12b04561e87ca53
Tejas Patil <patiltejas4604@gmail.com>
2023-01-29 12:52:19 +0530

    Demo_1

---
+++ Demoblaze/src/com/demoblaze/program/Demo1.java
@@ -0,0 +1,14 @@
+package com.demoblaze.program;
+
+public class Demo1 {
+	
+	public static void main(String[] args) {
+		
+		String name = "Demoblaze";
+		
+		System.out.println(new StringBuffer(name).reverse());
+		
+		System.out.println("Reverse String");
+	}
+
+}
